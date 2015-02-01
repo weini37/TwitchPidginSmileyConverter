29b5a48db0e28db02c75d5b2b6d0aada036c4131
Michael Weinberger <michael@weini.at>
2015-02-01 21:12:28 +0100

    Yeah now it does what it says

---
+++ src/at/weini/FileDownloader.java
@@ -0,0 +1,49 @@
+package at.weini;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class FileDownloader
+{
+	public static String downloadFile(String url) throws IOException
+	{
+		System.setProperty("http.agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
+		URL website = new URL(url);
+		URLConnection connection = website.openConnection();
+		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+		StringBuilder response = new StringBuilder();
+		String inputLine;
+
+		while ((inputLine = in.readLine()) != null)
+			response.append(inputLine);
+
+		in.close();
+
+		return response.toString();
+	}
+
+	public static void saveImage(String imageUrl, String destinationFile) throws IOException
+	{
+		URL url = new URL(imageUrl);
+		InputStream is = url.openStream();
+		OutputStream os = new FileOutputStream(destinationFile);
+
+		byte[] b = new byte[2048];
+		int length;
+
+		while ((length = is.read(b)) != -1)
+		{
+			os.write(b, 0, length);
+		}
+
+		is.close();
+		os.close();
+	}
+}
--- src/at/weini/Main.java
+++ src/at/weini/Main.java
@@ -1,9 +1,134 @@
 package at.weini;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class Main
 {
-	public Main()
-	{
 
+	/*
+	 * 
+	 * 
+	 * http://twitchemotes.com/api_cache/v2/global.json Global Emoticons
+	 * 
+	 * http://twitchemotes.com/api_cache/v2/subscriber.json Subscriber Emoticons
+	 * 
+	 * http://twitchemotes.com/api_cache/v2/sets.json Emote Set Mapping
+	 * 
+	 * http://twitchemotes.com/api_cache/v2/images.json Image ID Mapping
+	 */
+
+	public static void main(String[] args)
+	{
+		File dir = new File("/home/weini/.purple/smileys/subscriberTwitchSmileys/");
+		System.out.println("Checking if path exists " + dir.getPath());
+		System.out.println("Creating directory " + dir.getPath());
+		System.out.println(dir.mkdirs());
+
+		try
+		{
+			FileWriter writer = new FileWriter(new File(dir.getPath() + File.separator + "theme"));
+
+			writer.write("Name=Subscriber Twitch.tv Emotes\n");
+			writer.write("Description=All Subscriber Emotes from Twitch.tv\n");
+			writer.write("\n[default]\n");
+
+			GlobalDownload(writer, dir);
+			SubscriberDownload(writer, dir);
+
+			writer.close();
+		} catch (IOException e1)
+		{
+			e1.printStackTrace();
+		}
+	}
+
+	public static void SubscriberDownload(FileWriter writer, File dir)
+	{
+		String s = "";
+		try
+		{
+			s = FileDownloader.downloadFile("http://twitchemotes.com/api_cache/v2/subscriber.json");
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+			return;
+		}
+
+		JSONObject o = new JSONObject(s);
+
+		String imageiconURL = "http:" + o.getJSONObject("template").getString("small");
+
+		JSONObject channels = o.getJSONObject("channels");
+
+		int counter = 1;
+
+		for (String channelname : channels.keySet())
+		{
+			System.out.println(channelname);
+			JSONArray smileys = channels.getJSONObject(channelname).getJSONArray("emotes");
+			for (int j = 0; j < smileys.length(); j++)
+			{
+				JSONObject emote = (JSONObject) smileys.get(j);
+				try
+				{
+					System.out.println(counter + "/" + channels.length() + "\t" + emote.getString("code") + "\t" + imageiconURL.replace("{image_id}", "" + emote.getInt("image_id")));
+					if (!new File(dir.getPath() + File.separator + emote.getString("code")).exists())
+					{
+						FileDownloader.saveImage(imageiconURL.replace("{image_id}", "" + emote.getInt("image_id")), dir.getPath() + File.separator + emote.getString("code"));
+
+					}
+					writer.write(emote.getString("code") + "\t\t\t\t" + emote.getString("code") + "\n");
+				} catch (Exception e)
+				{
+					System.err.println("Can't download " + emote.getString("code"));
+				}
+			}
+			counter++;
+		}
+	}
+
+	public static void GlobalDownload(FileWriter writer, File dir)
+	{
+		String s = "";
+		try
+		{
+			s = FileDownloader.downloadFile("http://twitchemotes.com/api_cache/v2/global.json");
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+			return;
+		}
+
+		JSONObject o = new JSONObject(s);
+
+		String imageiconURL = "http:" + o.getJSONObject("template").getString("small");
+
+		JSONObject channels = o.getJSONObject("emotes");
+
+		int counter = 1;
+
+		for (String channelname : channels.keySet())
+		{
+			try
+			{
+				JSONObject emote = channels.getJSONObject(channelname);
+				System.out.println(counter + "/" + channels.length() + "\t" + channelname + "\t" + imageiconURL.replace("{image_id}", "" + emote.getInt("image_id")));
+				if (!new File(dir.getPath() + File.separator + channelname).exists())
+				{
+					FileDownloader.saveImage(imageiconURL.replace("{image_id}", "" + emote.getInt("image_id")), dir.getPath() + File.separator + channelname);
+				}
+				writer.write(channelname + "\t\t\t\t" + channelname + "\n");
+			} catch (Exception e)
+			{
+				e.printStackTrace();
+				System.err.println("Can't download " + channelname);
+			}
+			counter++;
+		}
 	}
 }
